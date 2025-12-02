2dc60df5ee08bd5a4df4942cbb81cfc8bb09bf6e
UmairFaiser <umairfaiser123@gmail.com>
2025-12-03 00:56:59 +0530

    fixing the deployement issure

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Explicitly set the root directory to avoid lockfile detection issues
-  experimental: {
-    turbo: {
-      root: ".",
-    },
-  },
 };
 
 export default nextConfig;
