58e4189b257a00f55fc51942c94bc90d708cc872
UmairFaiser <umairfaiser123@gmail.com>
2025-12-11 20:49:11 +0530

    fixed the deployement issue and the scrollable

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
   return (
-  <div className="min-h-screen bg-background">
+  <div className="min-h-screen bg-background overflow-hidden">
       <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
         <header className="mb-16 text-center">
           <Link href="/">
